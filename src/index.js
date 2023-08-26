c50433e68e50a8b09d1436fb1a046b7dbc45a44d
Maksim <maks.orehovskii@mail.ru>
2023-08-26 23:57:44 +0300

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketList = bracketsConfig.reduce((acc, curr) => {
+    acc.push(curr.join(''));
+    return acc;
+  }, []);
+  let originalStr = '\0';
+  while (str !== originalStr) {
+    originalStr = str;
+
+    bracketList.forEach(pair => {
+      str = str.replace(pair, '');
+    })
+  }
+
+  return str.length === 0;
 }
